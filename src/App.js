4bc688a3264aad32b9a6a642218e7b4fdbfbbfa1
VovaKatya <eka3973@yandex.ru>
2020-01-08 15:28:37 +0300

    add commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+    state = {
+        radioButtons: [
+            {id: 1, name: 'Yes', selected: true},
+            {id: 2, name: 'No', selected: false},
+            {id: 3, name: 'I don\'t know', selected: false}
+        ],
+    };
 
-export default App;
+    handleChange = ({target}) => {
+        const {radioButtons} = this.state;
+        const nexState = radioButtons.map(option => {
+            const checked = option.name === target.value;
+            return {
+                ...option,
+                selected: checked
+            }
+        });
+        this.setState({radioButtons: nexState})
+    };
+
+    render() {
+        const {radioButtons} = this.state;
+        return (
+            <div className='wrapper'>
+                <h4>An example of multiple radio buttons</h4>
+                <ul>
+                    {radioButtons.map(item => {
+                            return (
+                                <li key={item.id} className='item'>
+                                    <input type="radio"
+                                           value={item.name}
+                                           checked={!!item.selected}
+                                           onChange={this.handleChange}/>
+                                    {item.name}
+                                </li>
+                            )
+                        }
+                    )}
+                </ul>
+            </div>
+        );
+    }
+}
